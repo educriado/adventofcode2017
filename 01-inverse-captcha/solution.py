a36c708658913c1e13b753c5ad89fa764c1f0f28
Eduardo Criado Mascaray <ecriado95@gmail.com>
2017-12-05 02:42:39 +0100

    Solution for first part.

--- 01-inverse-captcha/solution.py
+++ 01-inverse-captcha/solution.py
@@ -1,16 +1,27 @@
 """Eduardo Criado - Code for first challenge of Advent of Code 2017."""
 
-def inverse_captcha(number):
+def inverse_captcha(num_list):
     """Return the inverse captcha value depending on the received number."""
-    return 0
+    i = 0
+    result = 0
+    while i < (len(num_list) - 1):
+        if num_list[i] == num_list[i + 1]:
+            result += num_list[i]
+        i += 1
+    #Check the last element
+    if num_list[0] == num_list[i]:
+        result += num_list[i]
+    return result
 
 def main():
     """Reads number from input file and prints result of inverse captcha."""
     input_file_path = "input.txt"
-    f = open(input_file_path, "r")
-    number = int(f.readline())
-    result = inverse_captcha(number)
-    print "The result using {} is {}".format(number, result)
+    num_file = open(input_file_path, "r")
+    num = int(num_file.readline())
+    #Turn num into array of digits
+    num_list = [int(i) for i in str(num)]
+    result = inverse_captcha(num_list)
+    print "The result using {} is {}".format(num, result)
 
 if __name__ == "__main__":
     main()
